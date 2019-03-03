f282806193579c0f1437dfc04ebde7cba7c9180e
graygabrielle <gray.atkinson.adams@gmail.com>
2019-03-02 16:48:08 -0800

    updates

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,14 +1,68 @@
 $(document).ready(function(){
+   
+    let crystalOne;
+    let crystalTwo;
+    let crystalThree;
+    let crystalFour;
+    let total = 0;
+    let target = 0;
+    let wins = 0;
+    let losses = 0;
 
-    $(".btn").on("click", function(event){
-        console.log(this);
-        console.log($(this));
 
-    });
+    function setTarget() {
+      let upperLimitScore = 120;
+      let lowerLimitScore = 19;
+      target = Math.floor(Math.random() * (upperLimitScore-lowerLimitScore)) + lowerLimitScore;
+      $("#target").html("Target: " + target);
+      // console.log(target);
+    }
 
-    $("#target").html("Target: ***");
-    $("#total").html("Total: ***");
-    $("#wins").html("Wins: ***");
-    $("#losses").html("Losses: ***");
 
-});
+   $(".btn").on("click", updateTotal);
+
+    function updateTotal (){
+      total += $(this).data("data-value");
+      $("#total").html("Total: " + total);
+      //total = total + this.data("data-value");
+    }
+
+    function crystalRandom() {
+      let crystalCeiling = 12;
+      let crystalFloor = 2;
+      let crystalValue = Math.floor(Math.random() * (crystalCeiling-crystalFloor)) + crystalFloor;
+      return crystalValue;
+    }  
+
+    function initializeReset (){
+      crystalOne = $("[data-id=1]");
+      crystalOne.data("data-value", crystalRandom()); 
+      console.log("Crystal 1: " + crystalOne.data("data-value"));
+      crystalTwo = $("[data-id=2]");
+      crystalTwo.data("data-value", crystalRandom()); 
+      console.log("Crystal 2: " + crystalTwo.data("data-value"));
+      crystalThree = $("[data-id=3]");
+      crystalThree.data("data-value", crystalRandom()); 
+      console.log("Crystal 3: " + crystalThree.data("data-value"));
+      crystalFour = $("[data-id=4]");
+      crystalFour.data("data-value", crystalRandom()); 
+      console.log("Crystal 4: " + crystalFour.data("data-value"));
+    
+      setTarget();
+      console.log("Target: " + target);
+      $("#total").html("Total: " + total);
+      $("#wins").html("Wins: " + wins);
+      $("#losses").html("Losses: " + losses);
+    }
+
+  function winLose() {
+    
+  }
+
+
+
+    initializeReset(); 
+    
+
+
+})
